a01d3fabcaaea0dc5feae5f0385d19454f9deb60
Adam Spannbauer <adamspannbauer@Adams-MacBook-Pro.local>
2020-11-30 19:26:40 -0500

    add initial logic

--- sketch.js
+++ sketch.js
@@ -1,13 +1,47 @@
-const canvasW = 512;
-const canvasH = 512;
+let drawing = false;
+let prevX;
+let prevY;
+
+function adjustedMouseXY() {
+  // relative to middle of screen
+  return [mouseX - width / 2, mouseY - height / 2];
+}
+
+function touchStarted() {
+  drawing = true;
+  [prevX, prevY] = adjustedMouseXY();
+}
+
+function touchEnded() {
+  drawing = false;
+}
 
 function setup() {
-  createCanvas(canvasW, canvasH);
+  createCanvas(windowWidth, windowHeight);
+  background(200);
+  strokeWeight(10);
+}
+
+function drawInWedges(x1, y1, x2, y2, nWedges) {
+  const da = TWO_PI / nWedges;
+  for (let i = 0; i < nWedges; i += 1) {
+    rotate(da * i);
+    line(x1, y1, x2, y2);
+  }
 }
 
 function draw() {
-  background(200);
+  translate(width / 2, height / 2);
+
+  if (drawing) {
+    const [x, y] = adjustedMouseXY();
+    drawInWedges(prevX, prevY, x, y, 8);
+    [prevX, prevY] = [x, y];
+  }
 }
 
 window.setup = setup;
 window.draw = draw;
+
+window.touchStarted = touchStarted;
+window.touchEnded = touchEnded;
