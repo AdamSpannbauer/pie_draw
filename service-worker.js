660c0335bb04ab3782586ea212b187bf5a4087d6
Adam Spannbauer <spannbaueradam@gmail.com>
2020-12-26 20:33:55 -0500

    make paths relative

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,34 @@
 const CACHE_NAME = 'v1::static';
 const urlsToCache = [
-  '/',
+  './',
 
-  '/site.webmanifest',
-  '/service-worker.js',
+  './site.webmanifest',
+  './service-worker.js',
 
-  '/style.css',
-  '/sketch.js',
+  './style.css',
+  './sketch.js',
 
   'https://cdn.jsdelivr.net/npm/p5@0.10.2/lib/p5.js',
   'https://cdn.jsdelivr.net/npm/simplify-js@1.2.4/simplify.min.js',
 
-  '/src/artUtils.js',
-  '/src/detectMobile.js',
-  '/src/paletteToolBarItem.js',
-  '/src/pathUtils.js',
-  '/src/toolBar.js',
-  '/src/toolBarItem.js',
-  '/src/wedgeBtn.js',
+  './src/artUtils.js',
+  './src/detectMobile.js',
+  './src/paletteToolBarItem.js',
+  './src/pathUtils.js',
+  './src/toolBar.js',
+  './src/toolBarItem.js',
+  './src/wedgeBtn.js',
 
-  '/assets/imgs/erase_all.png',
-  '/assets/imgs/save.png',
-  '/assets/imgs/undo.png',
+  './assets/imgs/erase_all.png',
+  './assets/imgs/save.png',
+  './assets/imgs/undo.png',
 
-  'android-chrome-192x192.png',
-  'android-chrome-512x512.png',
-  'apple-touch-icon.png',
-  'favicon-16x16.png',
-  'favicon-32x32.png',
-  'favicon.ico',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './apple-touch-icon.png',
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './favicon.ico',
 ];
 
 self.addEventListener('install', (event) => {
